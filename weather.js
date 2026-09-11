ae8ed19a32317fedf9bb35560df16357a4ef2b21
Tank1991333 <123tank456@gmail.com>
2026-09-11 11:07:05 +0200

    Add weather loading functionality with geolocation
    Fetch current weather data using geolocation and display temperature and wind speed.

--- weather.js
+++ weather.js
@@ -1,1 +1,42 @@
 
+function wetterLaden(){
+
+navigator.geolocation.getCurrentPosition(
+
+function(pos){
+
+fetch(
+`https://api.open-meteo.com/v1/forecast?latitude=${pos.coords.latitude}&longitude=${pos.coords.longitude}&current=temperature_2m,wind_speed_10m`
+)
+
+.then(r=>r.json())
+
+.then(data=>{
+
+document.getElementById(
+"temperature"
+).innerHTML =
+"🌡 Temperatur: "
++
+data.current.temperature_2m
++
+" °C";
+
+document.getElementById(
+"wind"
+).innerHTML =
+"💨 Wind: "
++
+data.current.wind_speed_10m
++
+" km/h";
+
+});
+
+}
+
+);
+
+}
+
+wetterLaden();
