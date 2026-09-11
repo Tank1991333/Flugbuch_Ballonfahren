11c9a6e0dfe08eff62f9af41325410b5065a007e
Tank1991333 <123tank456@gmail.com>
2026-09-11 11:06:49 +0200

    Set up map with markers and tile layer
    Initialize map with OpenStreetMap tiles and marker functions.

--- map.js
+++ map.js
@@ -1,1 +1,40 @@
 
+const map =
+L.map("map")
+.setView([47.285,15.98],10);
+
+L.tileLayer(
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+{
+attribution:"© OpenStreetMap"
+}
+).addTo(map);
+
+let startMarker = null;
+let landeMarker = null;
+
+function setStartMarker(lat,lng){
+
+if(startMarker){
+map.removeLayer(startMarker);
+}
+
+startMarker =
+L.marker([lat,lng])
+.addTo(map);
+
+map.setView([lat,lng],13);
+
+}
+
+function setLandingMarker(lat,lng){
+
+if(landeMarker){
+map.removeLayer(landeMarker);
+}
+
+landeMarker =
+L.marker([lat,lng])
+.addTo(map);
+
+}
