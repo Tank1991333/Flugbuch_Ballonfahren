c66471b611e437eca9796dd1e6fdb108f1039bf9
Tank1991333 <123tank456@gmail.com>
2026-09-11 11:59:18 +0200

    Refactor map.js to include satellite with labels

--- map.js
+++ map.js
@@ -1,16 +1,23 @@
 const osm = L.tileLayer(
-'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-attribution:'© OpenStreetMap'
-}
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 );
 
 const satellite = L.tileLayer(
-'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+);
+
+const labels = L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {
-attribution:'© Esri'
+opacity:0.35
 }
 );
+
+const satelliteWithLabels =
+L.layerGroup([
+satellite,
+labels
+]);
 
 const map = L.map('map', {
 center:[47.285,15.98],
@@ -18,38 +25,11 @@ zoom:10,
 layers:[osm]
 });
 
-const baseMaps = {
+L.control.layers({
+
 "🗺 Standard": osm,
-"🛰 Satellit": satellite
-};
 
-L.control.layers(baseMaps).addTo(map);
+"🛰 Satellit + Orte":
+satelliteWithLabels
 
-let startMarker = null;
-let landeMarker = null;
-
-function setStartMarker(lat,lng){
-
-if(startMarker){
-map.removeLayer(startMarker);
-}
-
-startMarker = L.marker([lat,lng])
-.addTo(map)
-.bindPopup("🎈 Start");
-
-map.setView([lat,lng],13);
-
-}
-
-function setLandingMarker(lat,lng){
-
-if(landeMarker){
-map.removeLayer(landeMarker);
-}
-
-landeMarker = L.marker([lat,lng])
-.addTo(map)
-.bindPopup("🏁 Landung");
-
-}
+}).addTo(map);
