a91579db88a103095f034a4c737f118b41a59e94
Tank1991333 <123tank456@gmail.com>
2026-09-11 11:57:58 +0200

    Refactor map initialization and marker handling

--- map.js
+++ map.js
@@ -1,14 +1,29 @@
-
-const map =
-L.map("map")
-.setView([47.285,15.98],10);
-
-L.tileLayer(
-"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+const osm = L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 {
-attribution:"© OpenStreetMap"
+attribution:'© OpenStreetMap'
 }
-).addTo(map);
+);
+
+const satellite = L.tileLayer(
+'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+{
+attribution:'© Esri'
+}
+);
+
+const map = L.map('map', {
+center:[47.285,15.98],
+zoom:10,
+layers:[osm]
+});
+
+const baseMaps = {
+"🗺 Standard": osm,
+"🛰 Satellit": satellite
+};
+
+L.control.layers(baseMaps).addTo(map);
 
 let startMarker = null;
 let landeMarker = null;
@@ -19,9 +34,9 @@ if(startMarker){
 map.removeLayer(startMarker);
 }
 
-startMarker =
-L.marker([lat,lng])
-.addTo(map);
+startMarker = L.marker([lat,lng])
+.addTo(map)
+.bindPopup("🎈 Start");
 
 map.setView([lat,lng],13);
 
@@ -33,8 +48,8 @@ if(landeMarker){
 map.removeLayer(landeMarker);
 }
 
-landeMarker =
-L.marker([lat,lng])
-.addTo(map);
+landeMarker = L.marker([lat,lng])
+.addTo(map)
+.bindPopup("🏁 Landung");
 
 }
